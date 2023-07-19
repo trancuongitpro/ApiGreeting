1ae62f9ced865d6e9d7692df83bf4e45e694df7b
ADMIN <trancuongitpro@gmail.com>
2023-07-19 15:59:53 +0700

    Initial commit

---
+++ src/main/java/com/example/apigreeting/ApiGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGreetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apigreeting/ApiGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
